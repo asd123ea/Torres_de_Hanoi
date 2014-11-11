4e25860eadad3e4bd99ce485cbfa495d0b65cac3
brunobck <brunobck@users.noreply.github.com>
2014-11-11 10:59:42 -0200

    Commit de inclusão de projeto.

---
+++ Hanoi/Movimento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    public class Movimento
+    {
+        public List<int> Pino1 { get; set; }
+        public List<int> Pino2 { get; set; }
+        public List<int> Pino3 { get; set; }
+
+        public Movimento()
+        {
+            Pino1 = new List<int>() 
+            { 
+                1,2,3
+            };
+
+            Pino3 = new List<int>();
+            Pino2 = new List<int>();
+
+        }
+
+        public int Mover(List<int> origem, List<int> destino)
+        {
+            destino.Add(origem[0]);
+            origem.RemoveAt(0);
+
+            return origem.Count;
+        }
+    }
+}
---
+++ HanoiTests/MovimentoTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hanoi;
+using NUnit.Framework;
+namespace Hanoi.Tests
+{
+    [TestFixture]
+    public class MovimentoTests
+    {
+        Movimento movimento;
+
+        [TestFixtureSetUp]
+        public void Movimentar()
+        {
+            movimento = new Movimento();
+        }
+        //Eliminamos tres testes para atender todos em um único teste. Verificamos se:
+        // o primeiro disco do pino foi removido;
+        //se ele foi movido para pino de destino;
+        // e se o disco foi removido do pino de origem.
+        [Test]
+        public void DeveMovimentarOPrimeiroDiscoDoPinoParaOutroPino()
+        {
+            movimento.Mover(movimento.Pino1, movimento.Pino3);
+            Assert.IsFalse(movimento.Pino1.Contains(1));
+            Assert.IsTrue(movimento.Pino3.Contains(1));
+            Assert.AreEqual(2, movimento.Pino1.Count);
+        }
+
+        [Test]
+        public void DeveMovimentarSegundoDiscoDaPilha()
+        {
+            movimento.Mover(movimento.Pino1, movimento.Pino3);
+            movimento.Mover(movimento.Pino1, movimento.Pino2);
+            Assert.IsFalse(movimento.Pino1.Contains(2));
+        }
+
+
+    }
+}
